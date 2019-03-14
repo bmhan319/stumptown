f608b4ad533912252334dadef914d632afd923b7
Brian Han <bmhan319@hotmail.com>
2019-03-14 16:23:51 -0700

    confirmation cart - made some timing tweaks

--- js/main.js
+++ js/main.js
@@ -24,16 +24,22 @@ function cartConfirm(index) {
   let confirm = document.querySelector("#cart-confirm")
   let cloneConfirm = confirm.cloneNode(true)
     
-    confirm.classList.add("confirm-animate")
-    confirm.style.backgroundColor = products[index].bgColor
-    setTimeout(function(){
-      confirm.parentNode.replaceChild(cloneConfirm, confirm)
-      cartCounter = cartCounter + 1
-      numItems.innerHTML = cartCounter
-    }, 3000);
-    
+  confirm.classList.add("confirm-animate")
+  confirm.style.backgroundColor = products[index].bgColor
+  cartCounter = cartCounter + 1
+  numItems.innerHTML = cartCounter
+
+  setTimeout(function(){
+    confirm.parentNode.replaceChild(cloneConfirm, confirm)      
+  }, 3000);
 }
 
+//Close Cart Confirmation
+function closeConfirm() {
+  let confirm = document.querySelector("#cart-confirm")
+  confirm.style.top = -90;
+  confirm.style.opacity = 0;
+}
 
 
 // Objects of Products
