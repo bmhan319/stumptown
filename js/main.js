9849e2896f642d986cbb05a2bbe7d9e9ae874d12
Brian Han <bmhan319@hotmail.com>
2019-03-14 00:27:08 -0700

    added scroll to fixed position on navbar in JS

--- js/main.js
+++ js/main.js
@@ -1,69 +1,18 @@
-// Opening/Closing Hamburger Menu on Mobile Page
-//Using JQuery for "toggle()"
-let navMenuCounter = 2;
-
-function openMenu(x, num) {
-  let navMobile = document.querySelector(".navbar-mobile")
+// Menu Bar to Fixed Position on Scoll
+document.addEventListener("scroll", fixedNav);
   
-  navMenuCounter = navMenuCounter + num;
-  if (navMenuCounter % 2 == 0) {
-    navMobile.style.display = "none";
-    navMenuCounter = 2
-    x.classList.toggle("change");
+function fixedNav() {
+  let height = innerHeight - 100
+  let head = document.querySelector("#header")
+  
+  if (scrollY >= height) {
+    head.style.position = "fixed"
+    head.style.top = 0
   } else {
-    navMobile.style.display = "block";
-    x.classList.toggle("change");
+    head.style.position = "relative"
+    head.style.top = -69
   }
 }
-
-
-// Bottom Popup Bar
-window.onscroll = function () {
-  let scrollPos = window.pageYOffset
-  if (scrollPos < 300) {
-    document.getElementById("contact-bar").style.bottom = "-84"
-  } else {
-    document.getElementById("contact-bar").style.bottom = "-10px"
-  }
-}
-
-
-// Code from "Slick Carousel"
-//http://kenwheeler.github.io/slick/
-//"Center Mode" Slider
-
-$(document).ready(function(){
-  $('.center-slide').slick({
-    centerMode: true,
-    centerPadding: '50px',
-    slidesToShow: 1,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
-});
-
-
-
-
-
 
 
 
