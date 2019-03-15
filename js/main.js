9c1702f69287977c2cde5cf9a74514a8de3aaab3
Brian Han <bmhan319@hotmail.com>
2019-03-15 01:24:53 -0700

    slight tweaks on nav page

--- js/main.js
+++ js/main.js
@@ -48,20 +48,17 @@ function openNav() {
   let body = document.querySelector("#bodyScroll")
   let nav = document.querySelector("#nav")
   let expand = document.querySelector("#expand")
-  console.log(expand)
   
   if (navCounter == true) {
     body.style.overflowY = "hidden"
-    nav.style.opacity = .98
-    nav.style.pointerEvents = "auto"
-    expand.style.transform = "scale(1)"
+    nav.classList.add("section-nav-open")
+    expand.classList.add("nav-row2-unexpand")
     navCounter = false
     
   } else if (navCounter == false) {
     body.style.overflowY = "visible"
-    nav.style.opacity = 0
-    nav.style.pointerEvents = "none"
-    expand.style.transform = "scale(1.4)"
+    nav.classList.remove("section-nav-open")
+    expand.classList.remove("nav-row2-unexpand")
     navCounter = true
   }
 }
