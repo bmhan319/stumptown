f3a55c8ba048540cd5f5de814f3650f7bd92030e
Brian Han <bmhan319@hotmail.com>
2019-03-29 15:41:24 -0700

    added the 'top' image to left col

---
+++ js/shop.js
@@ -0,0 +1,46 @@
+// Show/Hide "Top" link
+myID = document.getElementById("shopTop")
+
+var ScrollFunc = function() {
+  var y = window.scrollY;
+  if (y >= 900) {
+    myID.className = "shop-top-show"
+  } else {
+    myID.className = "shop-top-hide"
+  }
+}
+window.addEventListener("scroll", ScrollFunc)
+
+
+// Get 'Top' Image to stick to bottom of left col
+function locateFooter() {
+  let footer = document.querySelector("#footer")
+  let stickEl = document.querySelector(".shop-top-show")
+  let pos = footer.getBoundingClientRect().top - document.body.getBoundingClientRect().top
+  var window_bottom_position = window.scrollY + window.innerHeight
+
+  function getElementWindowTop(elem){
+    return elem && typeof elem.getBoundingClientRect === 'function' ? elem.getBoundingClientRect().top - document.body.getBoundingClientRect().top : 0;
+  }
+
+  var element_top_position = getElementWindowTop(footer)
+  var element_bottom_position = element_top_position + footer.clientHeight
+  
+  if(element_bottom_position >= window.scrollY && element_top_position <= window_bottom_position){
+    stickEl.className = "shop-top-show-absolute"
+  } else {
+    stickEl.className = "shop-top-show"
+  }
+}
+window.addEventListener("scroll", locateFooter)
+
+/*
+window.onresize = function() {
+  if (window.innerHeight >=750) {
+    document.querySelector(".shop-top-show").style.top = "90vh"
+  } else {
+    document.querySelector(".shop-top-show").style.top = "80vh"
+  }
+
+}
+*/
